2721e1d8bdbf0e03a50a6c4473c45029c2aeb2ab
Mikkel K Mosthaf <mosthaf@gmail.com>
2025-09-02 07:09:36 +0200

    Add user db

--- flask-sqlite3/app.py
+++ flask-sqlite3/app.py
@@ -3,12 +3,20 @@ import sqlite3
 
 # Import Flask and other necessary modules
 app = Flask(__name__)
-DATABASE = "beatles.db"
-def get_db():
+DB_ALBUM = "beatles.db"
+DB_USERS = "users.db"
+def get_db_albums():
     if "db" not in g:
-        g.db = sqlite3.connect(DATABASE)
+        g.db = sqlite3.connect(DB_ALBUM)
         g.db.row_factory = sqlite3.Row  # optional, for dict-like rows
     return g.db
+
+def get_db_users():
+    if "db" not in g:
+        g.db = sqlite3.connect(DB_USERS)
+        g.db.row_factory = sqlite3.Row  # optional, for dict-like rows
+    return g.db
+
 
 @app.teardown_appcontext
 def close_db(exception):
@@ -17,44 +25,21 @@ def close_db(exception):
         db.close()
 
 # Define the main route for the application
-@app.route("/")
-def front_page():
-    db = get_db()
+@app.route("/beatles")
+def beatles_page():
+    db = get_db_albums()
     cur = db.execute("SELECT title, yyear FROM album")
     data = cur.fetchall()
     members = {"members": [dict(u) for u in data]}
-    return render_template("index.html", title="Welcome", members=members)
+    return render_template("albums.html", title="Welcome", members=members)
 
-
-# Define routes for GET
-@app.route("/get")
-def get_page():
-    get_input = request.args.get("get_input", "10")
-    calculated_fib = fibonacci_number(int(get_input))
-    return render_template("http-get.html", get_input=get_input, calculated_fib=calculated_fib)
-
-# Define the route for POST requests
-@app.route("/post", methods=["GET", "POST"])
-def post_page():
-  if request.method == "POST":
-    post_input = request.form.get("post_input", "10")
-  else:
-    # Default value for GET request
-    post_input = "10"
-  calculated_fib = fibonacci_number(int(post_input))
-  return render_template("http-post.html", post_param=post_input, calculated_fib=calculated_fib)
-
-# Function to calculate Fibonacci number2
-def fibonacci_number(n):
-    if n <= 0:
-      return 0
-    elif n == 1:
-      return 1
-    else:
-      a, b = 0, 1
-      for _ in range(2, n + 1):
-        a, b = b, a + b
-      return b
+@app.route("/")
+def front_page():
+    user_db = get_db_users()
+    cur = user_db.execute("SELECT uid, uname, password FROM user")
+    data = cur.fetchall()
+    users = {"users": [dict(u) for u in data]}
+    return render_template("index.html", title="Welcome", users=users)
     
 # Start Flask server
 if __name__ == "__main__":
